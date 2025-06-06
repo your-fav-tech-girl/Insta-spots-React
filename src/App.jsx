c1b569cb1e7bc373285aed9d201fc8f0d5ab384c
Funmilola <funmilolaakanbi44@gmail.com>
2025-06-06 13:16:03 +0400

    fix case sensitive

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Logo from "./components/Logo"
-import Header from './components/header'
+import Header from './components/Header'
 import Content from './components/Content'
 import Footer from './components/Footer'
 import './App.css'
